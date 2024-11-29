ac6147b260321a2c4c7f20ce6d4b23a04b0ae66a
samuel pereira <samuelpereiramuleu@gmail.com>
2024-11-28 22:52:40 -0300

    refazendo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
-import { ReactNode } from 'react';
-import './globals.css';  
-import Link from 'next/link';
+import { ReactNode } from "react";
+import "./globals.css";
+import Link from "next/link";
 
 interface LayoutProps {
   children: ReactNode;
@@ -14,27 +14,20 @@ const Layout = ({ children }: LayoutProps) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>My Website</title>
-   
       </head>
-      <body className="flex flex-col min-h-screen">
-        <header className="bg-gray-800 text-white p-4">
-          <nav>
-            <ul className="flex space-x-4">
-              <Link href="/" className="hover:underline">Home</Link>
-              <li><a href="/movies" className="hover:underline">movies</a></li>
-              <li><a href="/contact" className="hover:underline">Contact</a></li>
-            </ul>
-          </nav>
-        </header>
-        <main className="flex-1 p-4">
-          {children} 
-        </main>
-        <footer className="bg-gray-800 text-white p-4 mt-auto">
-          <p>&copy; 2024 My Website</p>
+      <body className="flex flex-col bg-gradient-to-r from-slate-800 to-gray-900 min-h-screen">
+        <header className=" text-white p-4"></header>
+        <main className="flex-1 p-4 ">{children}</main>
+        <footer className=" text-white p-4 mt-auto">
+
+          <Link href="/home" className="hover:underline">Home</Link>
+          <Link href="/about" className="hover:underline">About</Link>
+          <Link href="/contact" className="hover:underline">Contact</Link>
+   
         </footer>
       </body>
     </html>
   );
-}
+};
 
 export default Layout;
--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,6 +2,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 interface Movies {
   id: number;
@@ -10,12 +15,20 @@ interface Movies {
   backdrop_path: string;
 }
 
-const Page = () => {
+interface Series {
+  id: number;
+  title: string;
+  vote_average: number;
+  backdrop_path: string;
+}
+
+const MovieCard = () => {
   const [movies, setMovies] = useState<Movies[]>([]);
+  const [series, setSeries] = useState<Series[]>([]);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchPopularMovies = async () => {
+    const fetchMovies = async () => {
       try {
         const response = await axios.get(
           `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
@@ -27,34 +40,88 @@ const Page = () => {
       }
     };
 
-    fetchPopularMovies();
+    fetchMovies();
   }, []);
-  console.log(movies);
 
-  if (error) return <div>{error}</div>;
+  useEffect(() => {
+    const fetchTv = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+        );
+        setSeries(response.data.results);
+      } catch (err) {
+        setError("Failed to fetch series. Please try again later.");
+        console.log(err);
+      }
+    };
+    fetchTv();
+  }, []);
+
+  if (error) return <div className="text-red-500">{error}</div>;
+
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">Popular Movies</h1>
-      <ul className="space-y-4">
-        {movies.map((movie) => (
-          <li key={movie.id} className="border p-4 rounded shadow">
-            <h2 className="text-lg font-semibold">{movie.title}</h2>
-            <p>Rating: {movie.vote_average}</p>
-            <div className="relative w-full h-64">
+    <div className="">
+      <p className="text-2xl font-bold mb-4 text-white opacity-35">Movies</p>
 
-            <Image
-              layout="fill"
-              objectFit="contain"
-              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-              alt={movie.title}
-              className="w-full  rounded"
-            />
-            </div>
-          </li>
-        ))}
-      </ul>
+      {/* Input */}
+      <div className="mb-6">
+        <input
+          type="text"
+          className="rounded-lg w-[268px] bg-background_input border-t-2 border-r-2 border-l-2 border-green-200 focus:outline-1 pl-8 py-1 focus:outline-green-200 px-2 placeholder:text-lg"
+          placeholder="Procure"
+        />
+      </div>
+
+      {/* Lista de Filmes */}
+      <Carousel className="flex mt-6 flex-wrap">
+        <CarouselContent className="">
+          {movies.map((movie) => (
+            <CarouselItem
+              key={movie.id}
+              className="basis-1/3 flex items-center justify-center flex-col"
+            >
+              <div className="relative w-24 h-40">
+                <Image
+                  fill
+                  sizes="(max-width: 768px) 100vw, 300px"
+                  src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                  alt={`Poster of ${movie.title}`}
+                  className="rounded-lg object-cover"
+                />
+              </div>
+              <div className="mt-2 text-white opacity-70">{movie.title}</div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
+      <p className="text-2xl font-bold mb-4 text-white opacity-35">Series</p>
+
+      {/* Lista de Séries */}
+      <Carousel className="flex mt-6 flex-wrap">
+        <CarouselContent className="">
+          {series.map((tv) => (
+            <CarouselItem
+              key={tv.id}
+              className="basis-1/3 flex items-center justify-center flex-col"
+            >
+              <div className="relative w-24 h-40">
+                <Image
+                  fill
+                  sizes="(max-width: 768px) 100vw, 300px"
+                  src={`https://image.tmdb.org/t/p/w500${tv.backdrop_path}`}
+                  alt={`Poster of ${tv.title}`}
+                  className="rounded-lg object-cover"
+                />
+              </div>
+              <div className="mt-2 text-white opacity-70">{tv.title}</div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </div>
   );
 };
 
-export default Page;
+export default MovieCard;
